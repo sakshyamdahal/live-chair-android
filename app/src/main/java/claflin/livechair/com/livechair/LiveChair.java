59d849f9459b0853561fdec3ecf7712c4d602904
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-02-07 05:09:13 -0500

    finish home page

--- app/src/main/java/claflin/livechair/com/livechair/LiveChair.java
+++ app/src/main/java/claflin/livechair/com/livechair/LiveChair.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class FindBarbers extends ActionBarActivity {
+public class LiveChair extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
